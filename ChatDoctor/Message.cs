188fbf11830d74b25ab1cf7fb672871a977095ee
kana2022 <kkanat94@gmail.com>
2023-04-17 18:04:16 +0600

    Add project files.

---
+++ ChatDoctor/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatDoctor
+{
+    internal class Message
+    {
+        public string SenderName { get; set; }
+        public string? ReceiverName { get; set; }
+        public string Content { get; set; }
+    }
+}
